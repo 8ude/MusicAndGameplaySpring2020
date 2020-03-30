e49da201945da12260a673f7f8ac610cc00a4af0
8ude <crbertelsen@gmail.com>
2020-03-30 10:53:52 -0400

    add basic music visualizer

---
+++ AudioVisualization/Assets/Scripts/AudioBlocks.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script works like a basic visualizer with two modes:
+//in the first mode, it acts like an oscilloscope, with each block representing a point in time (or "phase")
+//in the second mode, it shows the frequency data
+public class AudioBlocks : MonoBehaviour
+{
+    public enum VisualizerMode { scope, fft }
+    public VisualizerMode currentMode;
+    VisualizerMode previousMode;
+
+    //must be a power of 2!
+    public int ArraySize = 512;
+
+    public GameObject blockPrefab;
+
+    private GameObject[] blocksArray;
+
+    private float[] outputDataArray;
+    private float[] fftDataArray;
+
+    public float scalingFactorScope;
+    public float scalingFactorFFT;
+    public float blockSpacing;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        blocksArray = new GameObject[ArraySize];
+
+        outputDataArray = new float[ArraySize];
+        fftDataArray = new float[ArraySize * 2];
+
+        for (int i = 0; i < ArraySize; i ++)
+        {
+            blocksArray[i] = Instantiate(
+                blockPrefab,
+                new Vector3(blockSpacing * i, 0, 0),
+                Quaternion.identity);
+        }
+    }
+
+
+    void Update()
+    {
+        //update audio buffers;
+        audioSource.GetOutputData(outputDataArray, 0);
+        audioSource.GetSpectrumData(fftDataArray, 0, FFTWindow.Hanning);
+
+        //update our visuals based on what mode we're using
+        switch (currentMode)
+        {
+            case VisualizerMode.scope:
+                for (int i = 0; i < blocksArray.Length; i++)
+                {
+                    blocksArray[i].transform.localScale = new Vector3(
+                        1f,
+                        outputDataArray[i] * scalingFactorScope,
+                        1f);
+                }
+                break;
+            case VisualizerMode.fft:
+
+                for (int i = 0; i < blocksArray.Length; i++)
+                {
+                    blocksArray[i].transform.localScale = new Vector3(
+                        1f,
+                        fftDataArray[i] * scalingFactorFFT,
+                        1f);
+                }
+
+                break;
+                   
+        }
+    }
+
+
+}
---
+++ AudioVisualization/Assets/Scripts/SoundParticleGate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundParticleGate : MonoBehaviour {
+
+    public SpectrumAnalysis spectrumAnalysis;
+
+    public float volumeThreshold = 0.1f;
+    
+    public ParticleSystem particleSystem;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start() {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float SpectrumEnergy = spectrumAnalysis.GetWholeEnergy();
+
+        if (SpectrumEnergy > volumeThreshold) {
+            particleSystem.Play();
+        }
+    }
+}
